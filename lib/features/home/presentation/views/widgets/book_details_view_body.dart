d7f5a17f7bb9dffe46c204d1d7b647e8e9850f5d
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-05-20 18:02:05 +0300

    create box action

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -3,6 +3,7 @@ import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
+import 'books_action.dart';
 import 'custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -37,7 +38,9 @@ class BookDetailsViewBody extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 18.0),
-          const BookRating(mainAxisAlignment: MainAxisAlignment.center)
+          const BookRating(mainAxisAlignment: MainAxisAlignment.center),
+          const SizedBox(height: 37.0),
+          const BooksAction()
         ],
       ),
     );
