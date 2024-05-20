b7e9a2a61a9e745c79f9fc14af4957f01a6812a1
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-05-20 17:16:14 +0300

    add rating to book details and run in ios

--- lib/features/home/presentation/views/widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -4,14 +4,17 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../../../../core/utils/styles.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.start});
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(
           FontAwesomeIcons.solidStar,
+          size: 14.0,
           color: Color(0xFFFFDD4F),
         ),
         const SizedBox(width: 6.3),
@@ -20,10 +23,11 @@ class BookRating extends StatelessWidget {
           style: Styles.textStyle16,
         ),
         const SizedBox(width: 5),
-        Text(
-          '(254)',
-          style: Styles.textStyle14.copyWith(
-            color: const Color(0xFF707070),
+        Opacity(
+          opacity: .5,
+          child: Text(
+            '(254)',
+            style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
           ),
         ),
       ],
