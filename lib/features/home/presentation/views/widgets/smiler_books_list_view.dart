b66fb81e54d3d7c25b29a4c506940f0417606d69
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-06-08 17:01:34 +0300

    fetch similar books done

--- lib/features/home/presentation/views/widgets/smiler_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/smiler_books_list_view.dart
@@ -1,5 +1,9 @@
+import 'package:bookly/features/home/presentation/manger/similar_books_cubit/similar_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../core/widgets/custom_error_widget.dart';
+import '../../../../../core/widgets/custom_loading_indicator.dart';
 import 'custom_book_item.dart';
 
 class SmilerBooksListView extends StatelessWidget {
@@ -7,17 +11,29 @@ class SmilerBooksListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .15,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 5.0),
-            child: CustomBookImage(imageUrl: 'https://m.media-amazon.com/images/I/71zkPeFMA4L._AC_UF1000,1000_QL80_.jpg'),
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+      builder: (context, state) {
+        if (state is SimilarBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .15,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5.0),
+                  child: CustomBookImage(
+                      imageUrl:
+                          'https://m.media-amazon.com/images/I/71zkPeFMA4L._AC_UF1000,1000_QL80_.jpg'),
+                );
+              },
+            ),
           );
-        },
-      ),
+        } else if (state is SimilarBooksFailure) {
+          return CustomErrorWidget(errorMassage: state.errMassage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
